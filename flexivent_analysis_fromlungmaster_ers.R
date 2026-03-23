10736c07e6a4729d18f4f8290a96e280742814f2
Robert Maidstone <robert.maidstone@manchester.ac.uk>
2026-03-23 12:07:01 +0000

    flexivent other measures

--- flexivent_analysis_fromlungmaster_ers.R
+++ flexivent_analysis_fromlungmaster_ers.R
@@ -9,16 +9,24 @@ library(lme4)
 library(rstatix)
 library(purrr)
 
+source("functions.r")
+
+#par_val<-"Cull_FV_Rrs"
+par_val<-"Cull_FV_Crs"
+#par_val<-"Cull_FV_Rrs"
+
 # load data ---------------------------------------------------------------
 read.xlsx("data/LungFunctionMasterDataSet.xlsx",sheet=2) -> animal_data # need this as some animals not in LFmaster
 read.xlsx("data/LFmaster.xlsx",sheet=2) -> animal_data_2
 read.xlsx("data/LFmaster.xlsx",sheet=1,fillMergedCells = TRUE) -> LF_data2
 
+
+
 rbind(animal_data %>% dplyr::select(Animal.ID,Treatment,Genotype) %>% unique,animal_data_2 %>% dplyr::select(Animal.ID=Sample,Treatment,Genotype) %>% unique) %>% unique() %>%
   dplyr::mutate(Genotype=ifelse(Genotype=="HET","KO",Genotype)) %>% 
   unique -> animal_data_m
 ###
-LF_data2 %>% filter(Parameter=="Cull_FV_Rrs") %>% pivot_longer(cols=4:99,names_to = "Animal.ID") %>%
+LF_data2 %>% filter(Parameter==par_val) %>% pivot_longer(cols=4:99,names_to = "Animal.ID") %>%
   group_by(`Animal.ID`,ZT) %>%
   filter(Mch_Conc!=0) %>%
   merge(animal_data_m ,by="Animal.ID")-> LF_data2
@@ -28,15 +36,18 @@ LF_data2 %>% rename(Sample=Animal.ID,Mch_conc=Mch_Conc,Value=value) %>%
 
 LF_data2 %>% filter(!is.na(Value)) -> LF_data
 
+LF_data_noninvert <- LF_data
+LF_data$Value <- (LF_data$Value)^(-1)
+
 # mann whitney u on pairwise zts ----------------------------------
 
 mw_results<-dr_MWU_pairwise(LF_data)
 
 # dose response model -----------------------------------------------------
 
-param_formodel <- dr_fit(LF_data) #uncomment these lines to rerun dr curve fits - warning takes some time
-save(param_formodel,file = "data/drcmodelparams_flex_LFmaster.RData")
-load("data/drcmodelparams_flex_LFmaster.RData")
+param_formodel <- dr_fit(LF_data_noninvert) #uncomment these lines to rerun dr curve fits - warning takes some time
+save(param_formodel,file = "data/drcmodelparams_flex_LFmaster_ers.RData")
+load("data/drcmodelparams_flex_LFmaster_ers.RData")
 
 anova_pvals_upper <- dr_anova(param_formodel,"Upper","log10(params) ~ ZT * Treatment") # not used
 
@@ -44,10 +55,10 @@ anova_pvals_slope <- dr_anova(param_formodel,"Slope","log10(-params) ~ ZT * Trea
 
 # plotting dose response curve --------------------------------------------
 
-p1<-dr_plot(LF_data,anova_pvals_slope,mw_results,c(0.7,5.7),y_lab="Maximum Airway Resistance R<sub>rs</sub>(cm.H<sub>2</sub>O.s.ml<sup>-1</sup>)")
+p1<-dr_plot(LF_data,anova_pvals_upper,mw_results,c(0,80),y_lab="Maximum Airway Resistance R<sub>rs</sub>(cm.H<sub>2</sub>O.s.ml<sup>-1</sup>)")
 p1
 
-png("plots/flex_meth_dose_response_LFmaster.png", width = 2400, height = 1500, res = 300)  # adjust size/res as needed
+png("plots/flex_meth_dose_response_LFmaster_ers.png", width = 2400, height = 1500, res = 300)  # adjust size/res as needed
 grid.draw(
   p1
 )
@@ -57,14 +68,16 @@ dev.off()
 
 # AUC sinusoidal analysis -----------------------------------------------------
 
-rhy_plot(LF_data,"AUC",y_lim=c(-6,50),y_lab="AUC Airway Resistance R<sub>rs</sub>(cm.H<sub>2</sub>O.s.ml<sup>-1</sup>)") -> analysis_out
+rhy_plot(LF_data,"AUC",y_lim=c(-50,120),y_lab="AUC Airway Resistance R<sub>rs</sub>(cm.H<sub>2</sub>O.s.ml<sup>-1</sup>)") -> analysis_out
 
 p_auc <- analysis_out$combined
-ggsave(p_auc,filename="plots/flex_AUC_LFmaster.png",width=8,height=5)
+p_auc
+ggsave(p_auc,filename="plots/flex_AUC_LFmaster_ers.png",width=8,height=5)
 
 # Max sinusoidal analysis -----------------------------------------------------
 
-rhy_plot(LF_data,"Max",y_lim=c(-.1,1.3),y_lab="Max Airway Resistance R<sub>rs</sub>(cm.H<sub>2</sub>O.s.ml<sup>-1</sup>)") -> analysis_out
+rhy_plot(LF_data,"Max",y_lim=c(-1,4),y_lab="Max Airway Resistance R<sub>rs</sub>(cm.H<sub>2</sub>O.s.ml<sup>-1</sup>)") -> analysis_out
 
 p_max <- analysis_out$combined
-ggsave(p_max,filename="plots/flex_max_LFmaster.png",width=8,height=5)
+p_max
+ggsave(p_max,filename="plots/flex_max_LFmaster_ers.png",width=8,height=5)
